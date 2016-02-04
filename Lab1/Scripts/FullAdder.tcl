561f5bcd7d8ae7a74eca090a51bcf93616b07b9a
Daniel Olsen <dolsen@siu.edu>
2016-02-04 09:48:01 -0600

    Finish FullAdder script

--- Lab1/Scripts/FullAdder.tcl
+++ Lab1/Scripts/FullAdder.tcl
@@ -1,7 +1,7 @@
 # Daniel Olsen
 # FullAdder.tcl
 
-set myFiles [list ../HalfAdder.v ../FullAdder.v];
+set myFiles [list ./../HalfAdder.v ./../FullAdder.v];
 set basename FullAdder;
 set virtual 1;
 
@@ -42,6 +42,41 @@ if { $virtual == 0} {
 }
 set_clock_latency $myClkLatency_ns $myClk
 set_output_delay $myOutDelay_ns -clock $myClk [all_outputs]
+
+set_load $myOutputLoad [all_outputs]
+set_max_fanout $myMaxFanout [all_inputs]
+set_fanout_load 8 [all_outputs]
+set_wire_load_model -name 8000 -library saed90nm_max
+set_max_area 0
+set_clock_uncertainty -setup $mySetup [get_clocks $myClk]
+set_clock_uncertainty -hold $myHold [get_clocks $myClk]
+
+set_fix_multiple_port_nets -all -buffer_constants
+
+compile -map_effort medium -exact_map
+check_design
+
+set filebase [concat $basename$runname]
+set fileext .v
+write -format verilog -hierarchy -output [concat $filebase$fileext]
+set fileext .vio
+redirect [concat $filebase$fileext] { report_constraint -all_violators }
+set fileext .maxtiming
+redirect [concat $filebase$fileext] { report_timing -path full -delay max -nworst 5 }
+set fileext .mintiming
+redirect [concat $filebase$fileext] { report_timing -path full -delay min -nworst 5 }
+set fileext .qor
+redirect [concat $filebase$fileext] { report_qor -significant_digits 4 }
+
+
+
+
+
+
+
+
+
+
 
 
 
