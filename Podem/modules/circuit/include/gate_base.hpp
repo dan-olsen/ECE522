fc6a213fc3c9dcbfc3271492f2a50bacfb9e4ad7
Daniel Olsen <dolsen@siu.edu>
2016-02-27 16:32:30 -0600

    Change constructor of gate_base, add access methods and change to strings for ids

--- Podem/modules/circuit/include/gate_base.hpp
+++ Podem/modules/circuit/include/gate_base.hpp
@@ -8,21 +8,33 @@
 #include "gate_types.hpp"
 
 #include <vector>
+#include <iostream>
 
 class gate_base {
 public:
-    gate_base();
+    gate_base(const std::string& name, GATE_TYPE type);
     virtual ~gate_base();
 
     std::string name() const;
     GATE_TYPE type() const;
 
+    std::vector<std::string>& fan_in();
+    std::vector<std::string>& fan_out();
+
+    unsigned int fan_in_count();
+    unsigned int fan_out_count();
+
+    void add_fan_in(const std::string &id);
+    void add_fan_out(const std::string &id);
+
+    friend std::ostream &operator<<( std::ostream &output, const gate_base &g );
+
 protected:
     std::string _name;
     GATE_TYPE _type;
 
-    std::vector<int> _fan_in;
-    std::vector<int> _fan_out;
+    std::vector<std::string> _fan_in;
+    std::vector<std::string> _fan_out;
 };
 
 #endif //PODEM_GATE_HPP
--- Podem/modules/circuit/src/gate_base.cpp
+++ Podem/modules/circuit/src/gate_base.cpp
@@ -4,7 +4,13 @@
 
 #include "gate_base.hpp"
 
-gate_base::gate_base()
+gate_base::gate_base(const std::string& name, GATE_TYPE type)
+{
+    _name = name;
+    _type = type;
+}
+
+gate_base::~gate_base()
 {
 
 }
@@ -14,14 +20,72 @@ std::string gate_base::name() const
     return _name;
 }
 
-gate_base::~gate_base()
-{
-
-}
-
 GATE_TYPE gate_base::type() const
 {
     return _type;
 }
 
 
+void gate_base::add_fan_in(const std::string &id)
+{
+    _fan_in.push_back(id);
+}
+
+void gate_base::add_fan_out(const std::string &id)
+{
+    _fan_out.push_back(id);
+}
+
+std::vector<std::string> &gate_base::fan_in()
+{
+    return _fan_in;
+}
+
+std::vector<std::string> &gate_base::fan_out()
+{
+    return _fan_out;
+}
+
+unsigned int gate_base::fan_in_count() {
+    return _fan_in.size();
+}
+
+unsigned int gate_base::fan_out_count() {
+    return _fan_out.size();
+}
+
+std::ostream &operator<<( std::ostream &output, const gate_base &g )
+{
+    output << g._name;// << std::endl;
+    output << "\t\t" << gate_type_strings[g._type];// << std::endl;
+    output << "\t" <<  g._fan_in.size();
+    output << "\t" <<  g._fan_out.size();
+
+    output << "\t\t0\t0\t";
+
+    output << "\t{";
+    for (auto iter = g._fan_in.begin(); iter != g._fan_in.end(); iter++)
+    {
+        if (iter != g._fan_in.begin())
+        {
+            output << ", ";
+        }
+
+        output << *iter;
+    }
+    output << "}";// << std::endl;
+
+    output << "\t\t{";
+    for (auto iter = g._fan_out.begin(); iter != g._fan_out.end(); iter++)
+    {
+        if (iter != g._fan_out.begin())
+        {
+            output << ", ";
+        }
+
+        output << *iter;
+    }
+    output << "}";// << std::endl;
+
+    return output;
+}
