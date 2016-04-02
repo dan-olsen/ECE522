c2b027be1a1ad97615198261ba7d3b339dc7c0a3
Daniel Olsen <dolsen@siu.edu>
2016-04-02 14:26:45 -0500

    Add timing

--- Podem/modules/main/src/main.cpp
+++ Podem/modules/main/src/main.cpp
@@ -1,5 +1,7 @@
 #include "circuit.hpp"
 
+#include <chrono>
+
 const std::string usage = "Usage: %s [ options ] CIRCUIT\n";
 
 int main(int argc, char* const argv[])
@@ -11,11 +13,20 @@ int main(int argc, char* const argv[])
         exit(-1);
     }
 
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    start = std::chrono::system_clock::now();
     circuit c(argv[1]);
 
     c.print_header();
 
-    c.print_circuit();
+    //c.print_circuit();
+
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+
+    std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
 
     return 0;
 }
